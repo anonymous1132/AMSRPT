416d340168feca006596cd1ca8682dc62a5b8257
anonymous1132 <netease540889@126.com>
2018-11-20 08:58:55 +0800

    AMSRPT
    ReqRpt023

---
+++ AMS.CIM.Caojin.RPTWebApp/AMS.CIM.Caojin.RPTWebApp/Models/ViewModel/ReqRpt023TableEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AMS.CIM.Caojin.RPTWebApp.Models
+{
+    public class ReqRpt023TableEntity
+    {
+        public Dictionary<string,int> Reasons_Pcs { get; set; }
+
+
+        public string Department { get; set; }
+
+        public int Total_Pcs { get { return Reasons_Pcs.Values.Sum(); } }
+    }
+}
---
+++ AMS.CIM.Caojin.RPTWebApp/AMS.CIM.Caojin.RPTWebApp/Models/ViewModel/ReqRpt024Controller.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AMS.CIM.Caojin.RPTWebApp.Models;
+using AMS.CIM.Caojin.RPTLibrary;
+
+
+namespace AMS.CIM.Caojin.RPTWebApp.Controllers
+{
+    public class ReqRpt024Controller : Controller
+    {
+
+        private ReqRpt024MainViewModel MainViewModel = new ReqRpt024MainViewModel();
+        // GET: ReqRpt024
+        public ActionResult Index()
+        {
+            return View(MainViewModel);
+        }
+
+        private ReqRpt024TableViewModel tableViewModel;
+        public PartialViewResult GetTableByDepartment(ReqRpt024PostViewModel viewModel)
+        {
+            try
+            {
+                tableViewModel = new ReqRpt024TableViewModel(viewModel);
+                return PartialView(tableViewModel);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private ReqRpt024LotDetailTableViewModel lotDetailTableViewModel;
+        public PartialViewResult GetLotDetail(ReqRpt024LotDetailQueryPostViewModel viewModel)
+        {
+            try
+            {
+                lotDetailTableViewModel = new ReqRpt024LotDetailTableViewModel(viewModel);
+                return PartialView(lotDetailTableViewModel);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
